ef442203f5b06f8ed1206a5f31e76e2849dfdb5a
hinanshi4 <hinanshis4@gmail.com>
2021-08-14 01:32:11 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var scrollDown = document.querySelector("#scroll-down");
+var inputDate = document.querySelector("#input-date");
+var inputNumber = document.querySelector("#lucky-number");
+var checkButton = document.querySelector("#button");
+var displayMessage = document.querySelector("#display-message")
+
+scrollDown.addEventListener("click", scrollWin);
+checkButton.addEventListener("click", clickEventHandler);
+
+var numberToCheck = inputNumber.value;
+
+function scrollWin() {
+    window.scrollTo(0, 660);
+}
+
+function checkBirthdayIsLucky(){
+    var dateString = inputDate.value.replaceAll("-","");
+    var result = getSum(dateString);
+    if(result % inputNumber.value == 0){
+        document.getElementById("display-message").innerHTML = "CONGRATS! Your birthday is lucky"; 
+    }
+    else{
+    displayMessage.innerHTML = "SORRY! your birthday is not lucky";
+    }
+}
+
+function getSum(dateString)
+{
+    var sum=0;
+    for(let i = 0;i<dateString.length;i++){
+    sum = sum + Number(dateString.charAt(i))
+    }
+return sum;
+}
+
+
+
+function clickEventHandler(){
+    checkBirthdayIsLucky();
+}
