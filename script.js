44a600170c32bcadf887e902343b4c9e774fe457
hinanshi4 <hinanshis4@gmail.com>
2021-08-16 10:18:53 +0530

    feat: added privacy notice button
    can be cancelled by clicking on cross button

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ var inputDate = document.querySelector("#input-date");
 var inputNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#button");
 var displayMessage = document.querySelector("#display-message")
+var crossBtn = document.querySelector(".cross");
 
 scrollDown.addEventListener("click", scrollWin);
 checkButton.addEventListener("click", clickEventHandler);
+crossBtn.addEventListener("click", crossClickHandler);
+
+function crossClickHandler(){
+    document.querySelector(".second-first").style.display = "none";
+}
 
 var numberToCheck = inputNumber.value;
 
@@ -17,10 +23,10 @@ function checkBirthdayIsLucky(){
     var dateString = inputDate.value.replaceAll("-","");
     var result = getSum(dateString);
     if(result % inputNumber.value == 0){
-        document.getElementById("display-message").innerHTML = "CONGRATS! Your birthday is lucky"; 
+        document.getElementById("display-message").innerHTML = "CONGRATS! Your birthday is lucky🎉"; 
     }
     else{
-    displayMessage.innerHTML = "SORRY! your birthday is not lucky";
+    displayMessage.innerHTML = "SORRY! your birthday is not lucky😞";
     }
 }
 
