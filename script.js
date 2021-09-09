6f439b78aba3ae1d6ea274f726bee43fe1b65be4
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-09-09 10:29:21 +0530

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//referencing html elements
 var scrollDown = document.querySelector("#scroll-down");
 var inputDate = document.querySelector("#input-date");
 var inputNumber = document.querySelector("#lucky-number");
@@ -5,24 +6,25 @@ var checkButton = document.querySelector("#button");
 var displayMessage = document.querySelector("#display-message")
 var crossBtn = document.querySelector(".cross");
 
+//adding event listener to listen to a click event
 scrollDown.addEventListener("click", scrollWin);
 checkButton.addEventListener("click", clickEventHandler);
 crossBtn.addEventListener("click", crossClickHandler);
 
-function crossClickHandler(){
+function crossClickHandler(){ //privacy notice
     document.querySelector(".second-first").style.display = "none";
 }
 
-var numberToCheck = inputNumber.value;
+var numberToCheck = inputNumber.value; //value entered in input
 
-function scrollWin() {
+function scrollWin() { //for scroll to a certain pixel
     window.scrollTo(0, 660);
 }
 
-function checkBirthdayIsLucky(){
-    var dateString = inputDate.value.replaceAll("-","");
-    var result = getSum(dateString);
-    if(result % inputNumber.value == 0){
+function checkBirthdayIsLucky(){ 
+    var dateString = inputDate.value.replaceAll("-",""); //convert date into a string
+    var result = getSum(dateString); //sum is returned
+    if(result % inputNumber.value == 0){ // %to check divisibility
         document.getElementById("display-message").innerHTML = "CONGRATS! Your birthday is lucky🎉"; 
     }
     else{
@@ -31,16 +33,16 @@ function checkBirthdayIsLucky(){
 }
 
 function getSum(dateString)
-{
+{ //here order of the digits doesn't matter as they will be summed up
     var sum=0;
-    for(let i = 0;i<dateString.length;i++){
-    sum = sum + Number(dateString.charAt(i))
+    for(let i = 0;i<dateString.length;i++){ //to iterate over the string
+    sum = sum + Number(dateString.charAt(i)) //cal sum of digits in the string
     }
-return sum;
+return sum; 
 }
 
 
 
-function clickEventHandler(){
+function clickEventHandler(){ 
     checkBirthdayIsLucky();
 }
